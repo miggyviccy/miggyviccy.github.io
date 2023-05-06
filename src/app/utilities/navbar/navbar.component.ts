bd1cb506d8f25b95a9a79b371c295718fda530a5
miggyviccy <migvicsid@gmail.com>
2023-05-06 18:00:34 +0800

    feat(component): :zap: make all components viewable thru mobile

--- src/app/utilities/navbar/navbar.component.ts
+++ src/app/utilities/navbar/navbar.component.ts
@@ -1,21 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit {
+export class NavbarComponent{
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-  scrollToSection(sectionId: string): void {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({behavior: 'smooth'});
-    }
-  }
 
 }
