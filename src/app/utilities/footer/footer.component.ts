73ce36a4696d7c93d73906368081a348f94f79a2
miggyviccy <migvicsid@gmail.com>
2023-05-08 08:34:01 +0800

    feat(component): :sparkles: add contacts to the navbar

--- src/app/utilities/footer/footer.component.ts
+++ src/app/utilities/footer/footer.component.ts
@@ -1,19 +1,10 @@
-import { Component, OnInit  } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent implements OnInit {
-  constructor() { }
+export class FooterComponent{
 
-  ngOnInit(): void {
-  }
-  scrollToSection(sectionId: string): void {
-    const section = document.getElementById(sectionId);
-    if (section) {
-      section.scrollIntoView({behavior: 'smooth'});
-    }
-  }
 }
